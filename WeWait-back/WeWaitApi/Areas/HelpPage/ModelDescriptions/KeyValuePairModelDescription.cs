00f57d2cb6e650523bb84f73e203693d6c0d8a1e
BatchCode_ <maxime.fgrim@gmail.com>
2020-08-30 15:52:39 +0200

    Cleanup

--- WeWait-back/WeWaitApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
+++
@@ -1,9 +0,0 @@
-namespace WeWait_back_C.Areas.HelpPage.ModelDescriptions
-{
-    public class KeyValuePairModelDescription : ModelDescription
-    {
-        public ModelDescription KeyModelDescription { get; set; }
-
-        public ModelDescription ValueModelDescription { get; set; }
-    }
-}
