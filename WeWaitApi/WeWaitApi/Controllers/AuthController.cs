f6bfd6f26f8e19253df9a42b214eb715cec1388d
BatchCode_ <maxime.fgrim@gmail.com>
2020-09-26 20:43:06 +0200

    [CRITICAL] FIXED MAJOR ISSUES WITH CORS

--- WeWaitApi/WeWaitApi/Controllers/AuthController.cs
+++ WeWaitApi/WeWaitApi/Controllers/AuthController.cs
@@ -53,12 +53,32 @@ namespace WeWaitApi.Controllers
             return tokenJwt; // token
         }
 
+        // /api/Auth/User
         [HttpPost("User")]
         public async Task<ActionResult> Login([FromBody] User user)
         {
             ActionResult response = Unauthorized();
 
             var usr = await _context.User.FirstOrDefaultAsync(u => (u.Email == user.Email) && (u.Password == user.Password));
+
+            if (usr == null)
+            {
+                return response;
+            }
+
+            var token = GenerateJwtToken(usr);
+            response = Ok(new { Token = token, Message = "Success" });
+
+            return response;
+        }
+
+        // /api/Auth/Wewaiter
+        [HttpPost("Wewaiter")]
+        public async Task<ActionResult> LoginWewaiter([FromBody] User user)
+        {
+            ActionResult response = Unauthorized();
+
+            var usr = await _context.User.FirstOrDefaultAsync(u => (u.Email == user.Email) && (u.Password == user.Password) && (u.RoleId == 2));
 
             if (usr == null)
             {
--- WeWaitApi/WeWaitApi/Startup.cs
+++ WeWaitApi/WeWaitApi/Startup.cs
@@ -1,18 +1,12 @@
 using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
 using Pomelo.EntityFrameworkCore.MySql.Infrastructure;
 using Pomelo.EntityFrameworkCore.MySql.Storage;
@@ -47,6 +41,22 @@ namespace WeWaitApi
                         ValidateAudience = false
                     };
                 });
+
+            // For more infos about implementing CORS please read: https://docs.microsoft.com/fr-fr/aspnet/core/security/cors?view=aspnetcore-3.1
+            services.AddCors(options =>
+            {
+                options.AddDefaultPolicy(
+                    builder =>
+                    {
+                        // builder.WithOrigins("*")
+                        builder
+                        .AllowAnyOrigin()
+                        .AllowAnyHeader()
+                        .AllowAnyHeader();
+                    });
+            });
+
+            // services.AddCors(cors);
             services.AddControllers();
         }
 
@@ -57,6 +67,8 @@ namespace WeWaitApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseCors();
 
             app.UseHttpsRedirection();
 
